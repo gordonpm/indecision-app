403a674eff50ffd6b12937088fccf61b13becd8f
gordonpm <gordonm0815@gmail.com>
2019-12-16 19:57:20 -0800

    added styles for header

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-import './styles/styles.scss'
+import 'normalize.css/normalize.css'; // reset for all browsers
+import './styles/styles.scss' 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 // Component to display header (title and subtitle)
 import React from 'react';
 const Header = (props) => (
-        <div>
-            <h1>{props.title}</h1>
-            {props.subtitle && <h2>{props.subtitle}</h2>}
+        <div className="header">
+            <h1 className="header__title">{props.title}</h1>
+            {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
         </div>
 );
 
